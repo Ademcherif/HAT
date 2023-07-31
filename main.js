6ffbac1c3de5c6214cbe20d2e90ae4fd38d79388
Adem Cherif <ademcherif2004@gmail.com>
2023-07-31 21:18:50 +0100

    [feat] add img

--- main.js
+++ main.js
@@ -8,4 +8,21 @@ document.addEventListener("DOMContentLoaded", function() {
       ease: "power3.out"
     });
   });
+  const cardControllers = document.querySelectorAll("[data-card-controller]");
+
+cardControllers.forEach(controller => {
+  controller.addEventListener("click", (e) => {
+    const card = e.currentTarget.parentElement.parentElement;
+    const isVisible = card.dataset.visible;
+
+    if (isVisible === "false") {
+      card.setAttribute("data-visible", true);
+    } else {
+      card.setAttribute("data-visible", false);
+    }
+  })
+})
+
+
+
   
